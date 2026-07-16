b9c2c1a517f5d23cb2f9d7fb1d700abe9a789056
temitope <teajo99@gmail.com>
2026-07-16 21:23:18 +0100

    Add automatic EKS deployment

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,19 +2,10 @@ resource "aws_ecr_repository" "financial_api" {
 
   name = "financial-api"
 
-  image_tag_mutability = "MUTABLE"
-
   image_scanning_configuration {
-
     scan_on_push = true
-
   }
 
-  tags = {
-
-    Project = var.project_name
-
-  }
+  image_tag_mutability = "IMMUTABLE"
 
 }
-
